5db3aa1d6390884d37547d4e31393f1ad2807f09
kenapagituh <55676060+kenapagituh@users.noreply.github.com>
2023-09-11 11:22:27 +0700

    Update DIY_MIDI_Controller_Atmega328.ino

--- DIY_MIDI_Controller_Atmega328.ino
+++ DIY_MIDI_Controller_Atmega328.ino
@@ -5,41 +5,43 @@
 */
 
 //=======================================================================================
+#define off 1  // on MIDI MODE, off DEBUG MODE
 
-// #include <MIDI.h>
-// MIDI_CREATE_DEFAULT_INSTANCE();
+#ifdef on
+#include <MIDI.h>
+MIDI_CREATE_DEFAULT_INSTANCE();
+#endif
 
 //=======================================================================================
-// Tombol
-const byte JumlahTombol = 3;
-const byte PinTombol[JumlahTombol] = {2, 3, 4};
+// Tombol dan Potensiometer. Silahkan ubah disini.
+const byte JumlahTombol = 3;                                    // Tombolnya ada berapa?
+const byte PinTombol[JumlahTombol] = { 2, 3, 4 };               // Tombolnya terhubung ke pin berapa?
+const byte JumlahPotensiometer = 2;                             // Potensiometernya ada berapa?
+const byte PinPotensiometer[JumlahPotensiometer] = { A0, A1 };  // Potensiometernya terhubung ke pin berapa?
 
+//=======================================================================================
+// Function Tombol. Jangan diubah jika tidak perlu.
 int buttonCState[JumlahTombol] = {};
 int buttonPState[JumlahTombol] = {};
-
-unsigned long lastDebounceTime[JumlahTombol] = {0};
+unsigned long lastDebounceTime[JumlahTombol] = { 0 };
 unsigned long debounceDelay = 50;
 
 //=======================================================================================
-// Potensiometer
-const byte JumlahPotensiometer = 2;
-const byte PinPotensiometer[JumlahPotensiometer] = {A0, A1};
-
-int potCState[JumlahPotensiometer] = {0};
-int potPState[JumlahPotensiometer] = {0};
+// Function Potensiometer. Jangan diubah jika tidak perlu.
+int potCState[JumlahPotensiometer] = { 0 };
+int potPState[JumlahPotensiometer] = { 0 };
 int potVar = 0;
 
-byte midiCState[JumlahPotensiometer] = {0};
-byte midiPState[JumlahPotensiometer] = {0};
+byte midiCState[JumlahPotensiometer] = { 0 };
+byte midiPState[JumlahPotensiometer] = { 0 };
 
 int TIMEOUT = 300;
 byte varThreshold = 10;
 boolean potMoving = true;
-unsigned long PTime[JumlahPotensiometer] = {0};
-unsigned long timer[JumlahPotensiometer] = {0};
+unsigned long PTime[JumlahPotensiometer] = { 0 };
+unsigned long timer[JumlahPotensiometer] = { 0 };
 
 //=======================================================================================
-
 // MIDI
 byte midiCh = 1;
 byte note = 36;
@@ -49,44 +51,40 @@ byte cc = 1;
 // SETUP dan LOOP
 
 void setup() {
-
   Serial.begin(115200);
   for (int i = 0; i < JumlahTombol; i++) {
     pinMode(PinTombol[i], INPUT_PULLUP);
   }
-  
 }
 
 void loop() {
   tombol();
   potensiometer();
-
 }
 
 //=======================================================================================
 
 // Tombol
 void tombol() {
-
   for (int i = 0; i < JumlahTombol; i++) {
     buttonCState[i] = digitalRead(PinTombol[i]);
-
-
     if ((millis() - lastDebounceTime[i]) > debounceDelay) {
       if (buttonPState[i] != buttonCState[i]) {
         lastDebounceTime[i] = millis();
         if (buttonCState[i] == LOW) {
-
-//          MIDI.sendNoteOn(note + i, 127, midiCh); // note, velocity, channel
-
+#ifdef on
+          MIDI.sendNoteOn(note + i, 127, midiCh);  // note, velocity, channel
+#elif off
           Serial.print(i);
           Serial.println(": Tombol ditekan");
+#endif
         } else {
-          
-//          MIDI.sendNoteOn(note + i, 0, midiCh); // note, velocity, channel
-
+#ifdef on
+          MIDI.sendNoteOn(note + i, 0, midiCh);  // note, velocity, channel
+#elif off
           Serial.print(i);
           Serial.println(": Tombol dilepas");
+#endif
         }
         buttonPState[i] = buttonCState[i];
       }
@@ -98,7 +96,6 @@ void tombol() {
 
 // Potensiometer
 void potensiometer() {
-
   for (int i = 0; i < JumlahPotensiometer; i++) {
     potCState[i] = analogRead(PinPotensiometer[i]);
     midiCState[i] = map(potCState[i], 0, 1023, 0, 127);
@@ -109,21 +106,19 @@ void potensiometer() {
     timer[i] = millis() - PTime[i];
     if (timer[i] < TIMEOUT) {
       potMoving = true;
-    }
-    else {
+    } else {
       potMoving = false;
     }
-
     if (potMoving == true) {
       if (midiPState[i] != midiCState[i]) {
-
-//        MIDI.sendControlChange(cc + i, midiCState[i], midiCh);
+#ifdef on
+        MIDI.sendControlChange(cc + i, midiCState[i], midiCh);
+#elif off
         Serial.print("Pot: ");
         Serial.print(i);
         Serial.print(" ");
         Serial.println(midiCState[i]);
-        //Serial.print("  ");
-        
+#endif
         potPState[i] = potCState[i];
         midiPState[i] = midiCState[i];
       }
